30b3b7babfaa2681cac2d309c17409d9b92f0374
Mickael Bober <mickael.bober@gmail.com>
2020-10-21 10:16:37 +0200

    Add CollectionType to tags.

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +15,14 @@ class TaskType extends AbstractType
     {
         $builder
             ->add('description')
-            ->add('tags')
+            ->add('tags', CollectionType::class, [
+                'entry_type' => TagType::class,
+                'entry_options' => ['label' => false],
+                'by_reference' => false,
+                'allow_add' => true,
+                'allow_delete' => true
+            ])
+            ->add('save', SubmitType::class)
         ;
     }
 
